cdf24ab4bc91c8562b6042645561bee55dd9412f
Elias Dorneles <eliasdorneles@gmail.com>
2017-02-28 14:25:26 -0500

    code cleanup

--- app.py
+++ app.py
@@ -5,25 +5,40 @@ from android.widget import Button
 
 
 class MyApp:
-    def init_activity(self, activity):
-        print("init_activity", activity)
-        layout = android.widget.LinearLayout(activity)
+    def __init__(self):
+        self._activity = None
+
+    def link_activity(self, activity):
+        self._activity = activity
+
+    def onCreate(self):
+        layout = LinearLayout(self._activity)
         layout.setOrientation(LinearLayout.VERTICAL)
 
-        button1 = Button(activity)
+        button1 = Button(self._activity)
         button1.setText("Press me")
-        button2 = Button(activity)
+        button2 = Button(self._activity)
         button2.setText("Press me too!")
 
         layout.addView(button1)
         layout.addView(button2)
 
-        activity.setContentView(layout)
+        self._activity.setContentView(layout)
+
+    def onStart(self):
+        print('activity onStart', self._activity)
+
+    def onResume(self):
+        print('activity onResume', self._activity)
+
+    def something_else(self):
+        print('something_else')
 
 
 app = MyApp()
 activity = android.PythonActivity.setListener(app)
-app.init_activity(activity)
+
+app.link_activity(activity)
 
 # TODO: why this doesn't work?
 # app.activity = activity
